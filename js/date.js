16c2004c58196d6b99014da18f771a37ef4107e9
duvalldor <duv18001@byui.edu>
2021-04-24 20:43:31 -0700

    Commit10b

--- js/date.js
+++ js/date.js
@@ -1,2 +1,3 @@
 document.getElementById('lastmodified').innerHTML = "<p> Last Changed: " + document.lastModified + "</p>"
-const year = document.querySelector('#year');
+const year = document.querySelector('#copyrightyear');
+year.innerHTML = new Date().getFullYear();
