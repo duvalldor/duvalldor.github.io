8cbfd69983703cd4c95948a98026aa099e5a1ba9
duvalldor <duv18001@byui.edu>
2021-06-05 12:28:39 -0700

    Commit Noon 0605

--- lesson07/js/imagehandling.js
+++ lesson07/js/imagehandling.js
@@ -1,35 +1,39 @@
 //get all imgs with data-src attribute
-const imagesToLoad = document.querySelectorAll("img[data-src]");
+const imagesToLoad = document.querySelectorAll(`img[data-src]`);
 
-//optional parameters being set for the IntersectionalObserver
-const imageOptions = {
-    threshold: 1,
-    rootMargin: "0px 0px 50px 0px"
-};
-
+//replace the src with data-src and remove the attribute data-src.
 const loadImages = (image) => {
-    image.setAttribute('src', image.getAttribute('data-src'));
+    image.setAttribute(`src`, image.getAttribute(`data-src`));
     image.onload = () => {
-        image.removeAttribute('data-src');
+        image.removeAttribute(`data-src`);
     };
 };
+//optional parameters being set for the IntersectionalObserver
+//brought from line 27
+const imgOptions = {
+    rootMargin: `0px 0px 50px 0px`,
+    threshold: 1,
+};
 
-
-//first check to see if Intersection Observer is supported
-if ('IntersectionObserver' in window) {
-    const imgObserver = new IntersectionObserver((items, imgObserver) => {
-        items.forEach((item) => {
+//first check to see if Intersection Observer is supported by the brower
+if (`IntersectionObserver` in window) {
+    const imgObserver = new IntersectionObserver(items => {
+        items.forEach(item => {
+            if (item.isIntersecting) {
+                loadImages(item.target);
+                imgObserver.unobserve(item.target);
+            }
         });
-    },    imgOptions);
+    }, imgOptions);  //sends to line 12 - const imgO
     
     //Loop through each img and check status and load if necessary
-    imagesToLoad.forEach((img) => {
+    imagesToLoad.forEach(img => {
         imgObserver.observe(img);
     });
 }
 else {
     //just Load ALL images if not supported
-    imagesToLoad.forEach((img) => {
+    imagesToLoad.forEach(img => {
         loadImages(img);
     });
 }
