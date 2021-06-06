5cf5d24a78e1b8bd24f8c43ae98ddde24af7ab2e
duvalldor <duv18001@byui.edu>
2021-06-05 22:30:22 -0700

    final commit with visited this page

--- lesson07/js/date.js
+++ lesson07/js/date.js
@@ -8,3 +8,22 @@ document.getElementById('lastmodified').innerHTML = "<p> Last Changed: " + fulld
 
 const year = document.querySelector('#copyrightyear');
 year.innerHTML = new Date().getFullYear();
+
+//add to the users local storage # of visits
+if (localStorage.timesvisited == undefined) {
+    localStorage.timesvisited = 1;
+} else {
+    localStorage.timesvisited = Number(localStorage.timesvisited) + 1;
+}
+const timesvisited = `Welcome, you have visited this page: ${localStorage.timesvisited} times`;
+document.querySelector(`#timesvisited`).textContent = timesvisited;
+
+
+//days since last visit    sincelastvisit
+var dateoflastvisit = new Date();
+if (localStorage.dateoflastvisit != undefined) {
+    dateoflastvisit = localStorage.dateoflastvisit;
+}
+localStorage.dateoflastvisit = new Date();
+
+const sincelastvisit = new Date() - dateoflastvisit;
