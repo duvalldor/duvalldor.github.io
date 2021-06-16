77692b0c98a6ce47614bf1c2092721b8063843da
duvalldor <duv18001@byui.edu>
2021-06-15 21:10:09 -0700

    commit 0615 9:13pm

--- lesson09/js/prophets.js
+++ lesson09/js/prophets.js
@@ -1,25 +1,48 @@
 //source of prophet information in json
-const requestURL = 'https://github.com/byui-cit230/byui-cit230.github.io/blob/master/lessons/lesson-09/data/latter-day-prophets.json';
+const requestURL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json';
 
 fetch(requestURL)
     .then(function(response) {
         return response.json();
     })
     .then(function(jsonObject) {
-        console.tablel(jsonObject); //temporary checking for valid response and data parsing
+        
+        //console.tablel(jsonObject); //temporary checking for valid response and data parsing- which I can't make work
         //name the jsonObject
         const prophets = jsonObject['prophets']
-            //works through each prophet in the array of prophets
-        for (let i = 0; i < prophets.length; i++);
+        
+        //works through each prophet in the array of prophets
+        for (let i = 0; i < prophets.length; i++){
 
-            //name the different areas of the dom
-        let card = document
+        //name the different areas of the dom
+        let card = document.createElement('section');
+        let h2 = document.createElement('h2');
+        let bdate = document.createElement('p');
+        let bplace = document.createElement('p');
+        let eximage = document.createElement('img');
+
+        //set up what will be going in each of the create elements
+        h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
+        bdate.textContent = 'Date of Birth: ' + prophets[i].birthdate;
+        bplace.textContent = 'Place of Birth: ' + prophets[i].birthplace;
+        eximage.setAttribute('src', prophets[i].imageurl);
+        eximage.setAttribute('alt', prophets[i].name + ' ' + prophets[i].lastname + ' - ' + prophets[i].order);
+        
+        //tell the card the order you want the information placed
+        card.appendChild(h2);
+        card.appendChild(bdate);
+        card.appendChild(bplace);
+        card.appendChild(eximage);
+
+        //place in the 'card' the above information
+        document.querySelector('div.cards').appendChild(card);
+    }
     });
 
 
 
-// let card = document.createElement('section');
-// let h2 = document.createElement('h2');
+
+
 
 // h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
 
