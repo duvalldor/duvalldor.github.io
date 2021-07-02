be3c33e39f7ee6172818cf6be86b0f3808e04c54
duvalldor <duv18001@byui.edu>
2021-07-02 14:35:37 -0700

    Commit 0702 2:39

--- lesson11/js/towndata.js
+++ lesson11/js/towndata.js
@@ -1,8 +1,6 @@
 //source of towndata in json
 const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
 
-
-
 fetch(requestURL)
     .then(function(response) {
         return response.json();
